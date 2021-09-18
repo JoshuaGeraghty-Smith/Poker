1e099b9691193704cc9abdf03200d482c91f1d84
JoshuaGeraghty-Smith <90650841+JoshuaGeraghty-Smith@users.noreply.github.com>
2021-09-18 13:07:52 +0100

    Update rules.py

--- rules.py
+++ rules.py
@@ -2,7 +2,7 @@ from dataclasses import field, dataclass
 from typing import List
 
 @dataclass
-class players():
+class Players():
     players: List = field(default_factory=lambda: [])
     
     def set_players(self, player):
