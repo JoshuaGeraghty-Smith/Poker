7081595a2083ebe40b2f81393121fd0d64cc3d8c
JoshuaGeraghty-Smith <90650841+JoshuaGeraghty-Smith@users.noreply.github.com>
2021-09-18 13:31:21 +0100

    Update rules.py

--- rules.py
+++ rules.py
@@ -3,10 +3,14 @@ from typing import List
 
 @dataclass
 class Table():
+    
+#This class adds players to the table, lays community cards onto the table and 
+#adds chips to the pot
+
     players: List = field(default_factory=lambda: [])
     
     def set_players(self, player):
-        self.Table.append(player)
+        self.players.append(player)
         
     pot: int = 0
     
@@ -16,4 +20,4 @@ class Table():
     community_cards: List = field(default_factory=lambda: [])
     
     def lay_community_cards(card):
-        self.Table.append(card)
+        self.community_cards.append(card)
