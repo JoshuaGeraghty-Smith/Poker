6196587695bed56079cfd09a39a6fe089f316e65
JoshuaGeraghty-Smith <90650841+JoshuaGeraghty-Smith@users.noreply.github.com>
2021-09-18 13:22:29 +0100

    added community cards

--- rules.py
+++ rules.py
@@ -2,8 +2,18 @@ from dataclasses import field, dataclass
 from typing import List
 
 @dataclass
-class Table():
+class Tables():
     players: List = field(default_factory=lambda: [])
     
     def set_players(self, player):
-        self.players.append(player)
+        self.Tables.append(player)
+        
+    pot: int = 0
+    
+    def set_pot():
+        pass
+    
+    community_cards: List = field(default_factory=lambda: [])
+    
+        def lay_community_cards(card):
+            self.Tables.append(card)
