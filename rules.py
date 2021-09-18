a8be0e7f259e7703177c3583c5e64591c95c000b
JoshuaGeraghty-Smith <90650841+JoshuaGeraghty-Smith@users.noreply.github.com>
2021-09-18 13:36:45 +0100

    docs added

--- rules.py
+++ rules.py
@@ -3,14 +3,17 @@ from typing import List
 
 @dataclass
 class Table():
-    
-#This class adds players to the table, lays community cards onto the table and 
-#adds chips to the pot
+
+"""    
+This class adds players to the table, lays community cards onto the table and 
+adds chips to the pot
+"""
 
     players: List = field(default_factory=lambda: [])
     
     def set_players(self, player):
         self.players.append(player)
+        
         
     pot: int = 0
     
