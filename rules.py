3fbb5481b5f1a5369782753228f5efb4e0dda8b2
JoshuaGeraghty-Smith <90650841+JoshuaGeraghty-Smith@users.noreply.github.com>
2021-09-18 13:20:27 +0100

    Update rules.py

--- rules.py
+++ rules.py
@@ -2,13 +2,18 @@ from dataclasses import field, dataclass
 from typing import List
 
 @dataclass
-class players():
+class Tables():
     players: List = field(default_factory=lambda: [])
     
-    def set_players(self, player_name):
-        self.players.append(player)
+    def set_players(self, player):
+        self.Tables.append(player)
         
     pot: int = 0
     
     def set_pot():
         pass
+    
+    community_cards: List = field(default_factory=lambda: [])
+    
+        def lay_community_cards(card):
+            self.Tables.append(card)
